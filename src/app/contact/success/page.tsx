414d680b0b71af84df0346e1eabce54a2a84b090
RhythmS00d <rhythmsood1@gmail.com>
2024-02-26 13:05:37 +1100

    added success message animation

--- src/app/contact/success/page.tsx
+++ src/app/contact/success/page.tsx
@@ -1,8 +1,24 @@
+'use client'
+import { useState } from "react";
+
+import { MdEmail } from "react-icons/md";
+import { FaThumbsUp } from "react-icons/fa";
+import "animate.css"
+
 export default function page() {
+  const [startAnimation, setStartAnimation] = useState(false)
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="px-4 text-2xl font-semibold">Your message is sent</h1>
-      {/* <MdEmail className="size-40" /> */}
+    <div className="flex flex-col items-center justify-center overflow-hidden">
+      <h1 className="px-4 text-2xl font-semibold mb-4">Your message is sent</h1>
+      {!startAnimation && (
+        <MdEmail
+          className="size-40 animate__animated animate__backOutRight"
+          onAnimationEnd={() => setStartAnimation(true)}
+        />
+      )}
+      {startAnimation && (
+        <FaThumbsUp className="size-40 animate__animated animate__backInRight" />
+      )}
     </div>
   );
 }
